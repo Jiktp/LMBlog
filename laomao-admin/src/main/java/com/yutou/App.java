be980c774f4fac45e423beaf4c84c8b5525342d5
美味魔法鸡块 <dahumao123@outlook.com>
2025-10-23 13:11:25 +0800

    个人博客创建的准备工作

---
+++ laomao-admin/src/main/java/com/yutou/App.java
@@ -0,0 +1,13 @@
+package com.yutou;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ laomao-framework/src/test/java/com/yutou/AppTest.java
@@ -0,0 +1,19 @@
+package com.yutou;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testApp() {
+        // 简单测试示例
+        assertTrue(true);
+    }
+}

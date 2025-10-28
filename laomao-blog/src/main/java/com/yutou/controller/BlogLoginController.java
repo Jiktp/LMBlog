f5b4fb0982077ccc3b4e61a5ad5442d0df46a0fe
美味魔法鸡块 <dahumao123@outlook.com>
2025-10-28 10:42:23 +0800

    异常的统一处理

--- laomao-blog/src/main/java/com/yutou/controller/BlogLoginController.java
+++ laomao-blog/src/main/java/com/yutou/controller/BlogLoginController.java
@@ -2,8 +2,11 @@ package com.yutou.controller;
 
 import com.yutou.domain.ResponseResult;
 import com.yutou.domain.entity.User;
+import com.yutou.enums.AppHttpCodeEnum;
+import com.yutou.exception.SystemException;
 import com.yutou.service.BlogLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +19,12 @@ public class BlogLoginController {
     private BlogLoginService blogLoginService;
 
     @PostMapping("/login")
-    //ResponseResult是我们在huanf-framework工程里面写的实体类
+    //ResponseResult是我们在laomao-framework工程里面写的实体类
     public ResponseResult login(@RequestBody User user){
+        if(!StringUtils.hasText(user.getUserName())){
+            //提示 必须要传用户名
+            throw new SystemException(AppHttpCodeEnum.REQUIRE_USERNAME);
+        }
         return blogLoginService.login(user);
     }
 
